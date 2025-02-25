e914c18ae521919cf10e3e4a9c06171e8adc1bb8
Ankit Biswal <imankit.biswal43@gmail.com>
2025-02-25 18:58:51 +0530

    Modified and added Tailwind css

--- Frontend/src/pages/board/DashBoard.jsx
+++ Frontend/src/pages/board/DashBoard.jsx
@@ -26,11 +26,18 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard-container container">
-      <div className="dashboard-content container">
-        <h1 className="dashboard-title h1">Welcome, {username}!</h1>
+    <>
+        <div class="container px-4 text-center">
+          <div class="row gx-5">
+            <div class="col">
+              <div class="p-3">Custom column padding</div>
+      </div>
+        <div class="col">
+          <div class="p-3">Custom column padding</div>
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
